d4b80829776f3aa4c69c742c70eea40de807239e
Man-Of-May <bmanomay2018@gmail.com>
2025-03-26 06:02:24 +0000

    refined

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -4,16 +4,16 @@ interface User {
     userId: number,
     googleId?: string
     // startTrail: () => string
-    startTrail(): string
-    getCoupon(couponname: string, value: number): number
+    startTrail(): string// method that returns string
+    getCoupon(couponname: string, value: number): number// function that returns number
 }
 
 interface User {
     githubToken: string
 }
 
-interface Admin extends User {
-    role: "admin" | "ta" | "learner"
+interface Admin extends User { // interface => extends
+    role: "admin" | "ta" | "learner" // pipe function |, try to avoid it
 }
 
 const hitesh: Admin = { dbId: 22, email: "h@h.com", userId: 2211,
